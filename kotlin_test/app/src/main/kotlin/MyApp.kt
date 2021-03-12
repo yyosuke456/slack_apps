78da187120c0444b87692b35febeedaded188f05
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-03-12 09:12:08 +0900

    say-somethingを試すが、うまく動かず。どこかでコマンドを許可する必要がある？

--- kotlin_test/app/src/main/kotlin/MyApp.kt
+++ kotlin_test/app/src/main/kotlin/MyApp.kt
@@ -1,52 +1,70 @@
 import com.slack.api.bolt.App
-import com.slack.api.bolt.socket_mode.SocketModeApp
-import com.slack.api.model.event.MessageEvent
-import com.slack.api.model.kotlin_extension.view.blocks
-import com.slack.api.model.view.Views.*
+import com.slack.api.bolt.context.Context
+import com.slack.api.bolt.context.builtin.SlashCommandContext
+import com.slack.api.bolt.jetty.SlackAppServer
+import com.slack.api.methods.request.conversations.ConversationsListRequest
+import com.slack.api.model.ConversationType
 
 fun main() {
-  System.setProperty("org.slf4j.simpleLogger.log.com.slack.api", "debug")
 
-  // SLACK_BOT_TOKEN という環境変数が設定されている前提
-  val app = App()
+    // export SLACK_BOT_TOKEN=xoxb-***
+    // export SLACK_SIGNING_SECRET=123abc***
+    val app = App()
 
-  // イベント API
-  app.event(MessageEvent::class.java) { req, ctx ->
-    req.event.
-    ctx.say("こんにちは <@" + req.event.user + ">！")
-    ctx.ack()
-  }
-
-
-  // ショートカットとモーダル
-  app.globalShortcut("socket-mode-shortcut") { req, ctx ->
-    val modalView = view { v -> v
-      .type("modal")
-      .callbackId("modal-id")
-      .title(viewTitle { it.type("plain_text").text("タスク登録") })
-      .submit(viewSubmit { it.type("plain_text").text("送信") })
-      .close(viewClose { it.type("plain_text").text("キャンセル") })
-      .blocks {
-        input {
-          blockId("input-task")
-          element {
-            plainTextInput {
-              actionId("input")
-              multiline(true)
+    // requires channels:read scope
+    fun toConversationId(ctx: Context, where: String): String? {
+        val name = where.replaceFirst("#", "").trim()
+        var cursor: String? = null
+        var conversationId: String? = null
+        while (conversationId == null && cursor != "") {
+            val rb =
+                    ConversationsListRequest.builder().limit(1000).types(listOf(ConversationType.PUBLIC_CHANNEL))
+            val req = if (cursor != null) rb.cursor(cursor).build() else rb.build()
+            val list = ctx.client().conversationsList(req)
+            for (c in list.channels) {
+                if (c.name == name) {
+                    conversationId = c.id
+                    break
+                }
             }
-          }
-          label("タスクの詳細・期限などを書いてください")
         }
-      }
+        return conversationId
     }
-    ctx.asyncClient().viewsOpen { it.triggerId(req.payload.triggerId).view(modalView) }
-    ctx.ack()
-  }
-  app.viewSubmission("modal-id") { req, ctx ->
-    ctx.logger.info("Submitted data: {}", req.payload.view.state.values)
-    ctx.ack()
-  }
 
-  // SLACK_APP_TOKEN という環境変数が設定されている前提
-  SocketModeApp(app).start()
+    // requires channels:join scope
+    fun joinConversation(ctx: Context, conversationId: String) {
+        val res = ctx.client().conversationsJoin { it.channel(conversationId) }
+        ctx.logger.info("conversions.join result - {}", res)
+    }
+
+    fun saySomething(ctx: SlashCommandContext, where: String, text: String) {
+        val conversationId = toConversationId(ctx, where)
+        if (conversationId == null) {
+            ctx.ack("[Error] $where was not found")
+        } else {
+            joinConversation(ctx, conversationId)
+            val res = ctx.say { it.channel(where).text(text) }
+            ctx.logger.info("say result - {}", res)
+        }
+    }
+
+    app.command("/say-something") { req, ctx ->
+        val elements = req.payload.text?.split(" at ")
+        if (elements == null || elements.size < 2) {
+            ctx.ack("[Usage] /say-something Hey folks, how have you been doing? at #general")
+        } else {
+            if (elements.size == 2) {
+                val where = elements[1].trim()
+                val text = elements[0].trim()
+                saySomething(ctx, where, "$text by <@${req.payload.userId}>")
+            } else {
+                val where = elements[elements.size - 1].trim()
+                val text = elements.dropLast(1).joinToString { " at " }.trim()
+                saySomething(ctx, where, "$text by <@${req.payload.userId}>")
+            }
+            ctx.ack()
+        }
+    }
+    val server = SlackAppServer(app)
+    server.start()
 }
